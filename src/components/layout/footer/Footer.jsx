338e2b6b83f718007b67dcfe03bf76c63ed1a01b
unknown <yani.vilte@gmail.com>
2023-11-30 15:34:43 -0300

    pruebas en footer

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,35 +1,59 @@
 import "../footer/Footer.css"
+import eneBlanco from "../../../assets/eneBlanco.png"
+import naturalBlanco from "../../../assets/naturalBlanco.png"
+import twitter from "../../../assets/twitter.svg"
+import facebook from "../../../assets/facebook.svg"
+import instagram from "../../../assets/instagram.svg"
+import theme from "../../../temaConfig";
+import { ThemeProvider } from "@emotion/react";
+import { Typography } from "@mui/material";
+
 
 const Footer = () => {
+
     return (
+        <ThemeProvider theme={theme}>
         <footer className="pie-pagina">
             <div className="grupo-1">
                 <div className="box">
                     <figure>
                         <a href="#">
-                            <img src="img/logotipo-sleedw.svg" alt="Logo de SLee Dw" />
+                        <img src={eneBlanco} style={{ width: '303.351px', height: '302.212px', transform: 'rotate(-30deg)', flexShrink: 0, }} alt="Descripción de la imagen" />
                         </a>
                     </figure>
                 </div>
                 <div className="box">
-                    <h2>SOBRE NOSOTROS</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+                <img src={naturalBlanco} style={{ width: '144px', height: '24px', flexShrink: 0 }} alt="Descripción de la imagen" />
+                    <Typography variant="h6">Comida saludable congelada</Typography>
                 </div>
                 <div className="box">
-                    <h2>SIGUENOS</h2>
+                    <Typography variant="h4">Secciones</Typography>
+                    <Typography variant="h5">Home</Typography>
+                    <Typography variant="h5">Productos</Typography>
+                    <Typography variant="h5">Carrito</Typography>
+                    <Typography variant="h5">Perfil</Typography>
+                </div>
+                <div className="box">
+                <Typography variant="h4">Ayuda y soporte</Typography>
+                    <Typography variant="h5">Contactanos</Typography>
+                    <Typography variant="h5">Opciones de entrega</Typography>
+                    <Typography variant="h5">Términos y condiciones</Typography>
+                    <Typography variant="h5">Quienes somos</Typography>
+                </div>
+                <div className="box">
+                    <Typography variant="h4">Seguinos en nuestras redes</Typography>
                     <div className="red-social">
-                        <a href="#" className="fa fa-facebook"></a>
-                        <a href="#" className="fa fa-instagram"></a>
-                        <a href="#" className="fa fa-twitter"></a>
-                        <a href="#" className="fa fa-youtube"></a>
+                        <a href="#" className="fa fa-instagram"><img src={instagram} style={{ width: '24px', height: '24px' }} alt="Descripción de la imagen" /></a>
+                        <a href="#" className="fa fa-twitter"><img src={twitter} style={{ width: '24px', height: '24px' }} alt="Descripción de la imagen" /></a>
+                        <a href="#" className="fa fa-facebook"><img src={facebook} style={{ width: '24px', height: '24px' }} alt="Descripción de la imagen" /></a>
                     </div>
                 </div>
             </div>
             <div className="grupo-2">
-                <small>&copy; 2021 <b>SLee Dw</b> - Todos los Derechos Reservados.</small>
+                <small>&copy; 2023 <b>Equipo CH</b> - Todos los derechos reservados.</small>
             </div>
         </footer>
+        </ThemeProvider>
     );
 }
 
--- src/temaConfig.js
+++ src/temaConfig.js
@@ -2,21 +2,91 @@ import { createTheme } from "@mui/material";
 
 const theme = createTheme({
   typography: {
-    fontFamily: ['Agbalumo'].join(','),
+    fontFamily: ['Lexend'].join(','),
     h3: {
-      fontSize: '1.2rem',
-      '@media (min-width:600px)': {
-        fontSize: '1.5rem',
+      color: '#FFFFFF',
+      fontWeight: 500,
+      // '@media (min-width: 320px) and (max-width: 480px)': {
+      //   fontSize: '1.5rem',
+      // },
+      // '@media (min-width: 481px) and (max-width: 768px)': {
+      //   fontSize: '2.4rem',
+      // },
+      // '@media (min-width: 769px) and (max-width: 1024px)': {
+      //   fontSize: '3rem',
+      // },
+      // '@media (min-width: 1025px) and (max-width: 1200px)': {
+      //   fontSize: '4rem',
+      // },
+      // '@media (min-width: 1201px)': {
+      //   fontSize: '4rem',
+      // },
+    },
+    h4: {
+      color: '#FFFFFF',
+      fontWeight: 400,
+      // '@media (min-width: 320px) and (max-width: 480px)': {
+      //   fontSize: '0.8rem',
+      // },
+      // '@media (min-width: 481px) and (max-width: 768px)': {
+      //   fontSize: '1rem',
+      // },
+      // '@media (min-width: 769px) and (max-width: 1024px)': {
+      //   fontSize: '1.2rem',
+      // },
+      // '@media (min-width: 1025px) and (max-width: 1200px)': {
+      //   fontSize: '1.4rem',
+      // },
+      // '@media (min-width: 1201px)': {
+      //   fontSize: '1.4rem',
+      // },
+    },
+    h5: {
+      color: '#FFFFFF',
+      fontWeight: 300,
+      // '@media (min-width: 320px) and (max-width: 480px)': {
+      //   fontSize: '0.6rem',
+      // },
+      // '@media (min-width: 481px) and (max-width: 768px)': {
+      //   fontSize: '0.8rem',
+      // },
+      // '@media (min-width: 769px) and (max-width: 1024px)': {
+      //   fontSize: '1.0rem',
+      // },
+      // '@media (min-width: 1025px) and (max-width: 1200px)': {
+      //   fontSize: '1.2rem',
+      // },
+      '@media (min-width: 1201px)': {
+        fontSize: '1.2rem',
       },
-      '@media (min-width:960px)': {
-        fontSize: '2.4rem',
+    },
+    h6: {
+      color: '#FFFFFF',
+      fontWeight: 400,
+      // '@media (min-width: 320px) and (max-width: 480px)': {
+      //   fontSize: '0.5rem',
+      // },
+      // '@media (min-width: 481px) and (max-width: 768px)': {
+      //   fontSize: '0.6rem',
+      // },
+      // '@media (min-width: 769px) and (max-width: 1024px)': {
+      //   fontSize: '0.7rem',
+      // },
+      '@media (min-width: 1025px) and (max-width: 1200px)': {
+        fontSize: '0.8rem',
       },
-      '@media (min-width:1280px)': {
-        fontSize: '3rem',
+      '@media (min-width: 1201px)': {
+        fontSize: '0.8rem',
       },
-      '@media (min-width:1920px)': {
-        fontSize: '4rem',
-      },
+    },
+    
+  },
+  palette: {
+    primary: {
+      main: '#164439',
+    },
+    secondary: {
+      main: '#00ff00',
     },
   },
 });
