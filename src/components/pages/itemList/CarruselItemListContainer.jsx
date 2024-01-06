035a4c005dc2ed62094e6ae5f15f4977802168cf
Yanina Vilte <yani.vilte@gmail.com>
2024-01-06 16:15:50 -0300

    ultimo commit

--- src/components/pages/itemList/CarruselItemListContainer.jsx
+++ src/components/pages/itemList/CarruselItemListContainer.jsx
@@ -15,24 +15,24 @@ const CarouselItemListContainer = () => {
                     <Typography variant="carrusel">Estos son los productos que nuestros clientes adoran.</Typography>
                     <Typography variant="carrusel" sx={{ fontWeight: "600" }} >¿Cuál será tu próximo favorito?</Typography>
                 </div>
-                <img src={productosEstrellas} alt="Descripción de la imagen" />
+                <img src={productosEstrellas} alt="Productos Estrellas" />
             </div>
-            {/* <div style={{ background: "#164439", width: "100%", height: "633px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", width: "443px", height: "105px" }}>
+            <div className="carrusel">
+                <div className="carruselContainer">
                     <Typography variant="h1" sx={{ fontSize: "2.5rem", marginBottom: "30px" }}>¡Envíos express!</Typography>
                     <Typography variant="carrusel">Hacemos que comer saludable sea aún más fácil.</Typography>
                     <Typography variant="carrusel" sx={{ fontWeight: "600" }} >Compra ahora y disfruta del envío gratis en productos seleccionados.</Typography>
                 </div>
-                <img src={enviosExpress} style={{ width: "532px", height: "369px", borderTopRightRadius: "200px", borderBottomLeftRadius: "200px", marginLeft: "60px" }} alt="Descripción de la imagen" />
+                <img src={enviosExpress} alt="Envíos Express" />
             </div>
-            <div style={{ background: "#164439", width: "100%", height: "633px", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", width: "443px", height: "105px" }}>
+            <div className="carrusel">
+                <div className="carruselContainer">
                     <Typography variant="h1" sx={{ fontSize: "2.5rem", marginBottom: "30px" }}>Atención personalizada</Typography>
                     <Typography variant="carrusel">Estamos aquí para asegurarnos de que tu experiencia con nosotros sea excepcional en cada paso del camino y <Typography variant="carrusel" sx={{ fontWeight: "600" }} >estamos comprometidos a superar tus expectativas.</Typography></Typography>
                     
                 </div>
-                <img src={atencionPersonalizada} style={{ width: "532px", height: "369px", borderTopRightRadius: "200px", borderBottomLeftRadius: "200px", marginLeft: "60px" }} alt="Descripción de la imagen" />
-            </div> */}
+                <img src={atencionPersonalizada} alt="Atención Personalizada" />
+            </div>
         </div>
     );
 };
